1670eeeb36bd0cb13288119220e1ced4b6ec6761
alonso.reynoso <alonso.reynoso@bbva.com>
2020-10-24 16:30:43 -0500

    Initial commit

---
+++ src/main/java/pe/com/dummy4dummies/Dummy4DummiesApplication.java
@@ -0,0 +1,13 @@
+package pe.com.dummy4dummies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dummy4DummiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dummy4DummiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/dummy4dummies/Dummy4DummiesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.dummy4dummies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dummy4DummiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
